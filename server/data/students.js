ed26b01c445199b296f151a9d9a3bb2c1bb8f018
Akshay Pawar <127084513+Sakshay2503@users.noreply.github.com>
2024-03-31 13:20:20 +0530

    first commit

---
+++ server/data/students.js
@@ -0,0 +1,17 @@
+const students = [
+
+    {
+        name: "John Doe",
+        address: "123 Main Street",
+        category: "Science",
+        city: "New York",
+        contact: 1234567890,
+        fatherContact: 9876543210,
+        image: "https://example.com/images/john_doe.jpg",
+        roomNo: "101",
+        blockNo: "A",
+        status: "Hostel"
+      }
+      
+];
+export default students;
